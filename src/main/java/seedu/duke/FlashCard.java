5102ebf8c619f898464ef75bc1800f9394dff869
Wei Yin, Joanne Wong <jwweiyin@gmail.com>
2021-10-02 15:34:09 +0800

    Add add function
    Added parser to process program logic, FlashCard and FlashCardManager classes. Added ability to add flash cards to the flashcard manager, along with NoSlashException and FieldEmptyException exceptions to be thrown if the format of the command is incorrect.

---
+++ src/main/java/seedu/duke/FlashCard.java
@@ -0,0 +1,13 @@
+package seedu.duke;
+
+public class FlashCard {
+    private String front;
+    private String back;
+
+    public FlashCard(String front, String back) {
+        this.front = front;
+        this.back = back;
+    }
+
+
+}
---
+++ src/main/java/seedu/duke/Parser.java
@@ -0,0 +1,25 @@
+package seedu.duke;
+
+import java.util.Scanner;
+
+public class Parser {
+
+    static void programLogic() {
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        String word = line.split(" ")[0];
+        while (!word.equals("bye")) {
+            switch(word) {
+            case "add":
+                FlashCardManager.prepareToAddFlashCard(line);
+                break;
+            default:
+                System.out.println("\tThat's not a command.");
+            }
+            line = in.nextLine();
+            word = line.split(" ")[0];
+        }
+
+
+    }
+}
