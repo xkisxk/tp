24c642ae30809fe86319029fbb62363fed03d099
xRossKoh <e0543512@u.nus.edu>
2021-10-08 13:21:47 +0800

    Added AnswerTest.java for Answer class

---
+++ src/test/java/seedu/duke/AnswerTest.java
@@ -0,0 +1,15 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AnswerTest {
+
+    @Test
+    public void testGetAnswer(){
+        assertEquals("testAnswer",
+                new Answer("testAnswer", 1).getAnswer());
+    }
+
+}
