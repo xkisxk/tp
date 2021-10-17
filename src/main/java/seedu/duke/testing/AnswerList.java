5dd6853c7b3ded68b10459a4ae57833ced4ef97f
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-10-18 00:15:52 +0800

    Added testing units

--- src/main/java/seedu/duke/testing/AnswerList.java
+++ src/main/java/seedu/duke/testing/AnswerList.java
@@ -4,16 +4,25 @@ import seedu.duke.flashcard.Deck;
 import seedu.duke.flashcard.FlashCard;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class AnswerList {
     private ArrayList<Answer> answerList;
     private Deck deck;
+    private static final Logger logger = Logger.getLogger(Deck.class.getName());
 
     public AnswerList(Deck deck) {
         this.answerList = new ArrayList<>();
         this.deck = deck;
     }
 
+    /**
+     * Gets the index of the answer in the list.
+     *
+     * @param answer answer query
+     * @return       index of the answer
+     */
     public int getAnswerIndex(Answer answer) {
         return answerList.indexOf(answer);
     }
@@ -44,14 +53,22 @@ public class AnswerList {
         answerList.add(new Answer(answer, questionIndex));
     }
 
+    /**
+     * Gets the score of the particular test.
+     *
+     * @return score of the test
+     */
     public int getScore() {
+        logger.setLevel(Level.WARNING);
+        logger.log(Level.INFO, "Starting getScore process");
         int score = 0;
         for (Answer response : answerList) {
             int responseNumber = getAnswerIndex(response);
             FlashCard question = deck.getCard(responseNumber);
             String userAnswer = response.getAnswer();
-            
+
             if (response.isCorrect(userAnswer, question)) {
+                logger.log(Level.INFO, "Answer is correct");
                 score++;
             }
         }
--- src/test/java/seedu/duke/TestManagerTest.java
+++ src/test/java/seedu/duke/TestManagerTest.java
@@ -1,23 +1,4 @@
 package seedu.duke;
 
-import org.junit.jupiter.api.Test;
-import seedu.duke.exceptions.FieldEmptyException;
-import seedu.duke.parser.TestParser;
-import seedu.duke.testing.TestManager;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class TestManagerTest {
-
-    @Test
-    public void parseUserResponse_noInput_expectFieldEmptyException() {
-        String input = "";
-        assertThrows(FieldEmptyException.class, () -> TestParser.parseUserResponse(input));
-    }
-
-    @Test
-    public void toInt_invalidInput_expectNumberFormatException() {
-        String input = "a";
-        assertThrows(NumberFormatException.class, () -> TestParser.toInt(input));
-    }
 }
