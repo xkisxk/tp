47fe100ba6a830485d5b8331a60799ca332fcecb
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-10-17 13:43:21 +0800

    Added constructors for the exceptions

--- src/main/java/seedu/duke/exceptions/FieldEmptyException.java
+++ src/main/java/seedu/duke/exceptions/FieldEmptyException.java
@@ -1,8 +1,14 @@
 package seedu.duke.exceptions;
 
-
 /**
  * Represents exception thrown when a field that is expected to be filled is empty.
  */
 public class FieldEmptyException extends CardLiException {
+    public FieldEmptyException() {
+        super();
+    }
+
+    public FieldEmptyException(String message) {
+        super(message);
+    }
 }
--- src/main/java/seedu/duke/exceptions/NoSlashException.java
+++ src/main/java/seedu/duke/exceptions/NoSlashException.java
@@ -3,5 +3,12 @@ package seedu.duke.exceptions;
 /**
  * Represents exception thrown when user input does not contain '/' character when invoking certain commands.
  */
-public class NoSlashException extends CardLiException{
+public class NoSlashException extends CardLiException {
+    public NoSlashException() {
+        super();
+    }
+
+    public NoSlashException(String message) {
+        super(message);
+    }
 }
