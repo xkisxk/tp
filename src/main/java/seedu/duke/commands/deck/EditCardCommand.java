98b66aa8404122aa8c1949ec71825218ad25f0cc
ThaddeusLim99 <lcqthad@gmail.com>
2021-10-26 17:13:25 +0800

    abstracted out EditCardCommand, MoveCardCommand, EditDeckCommand

--- src/main/java/seedu/duke/commands/deck/EditCardCommand.java
+++ src/main/java/seedu/duke/commands/deck/EditCardCommand.java
@@ -9,6 +9,10 @@ import seedu.duke.flashcard.Deck;
 import seedu.duke.flashcard.FlashCard;
 import seedu.duke.parser.Parser;
 import seedu.duke.parser.deck.EditCardParser;
+import seedu.duke.testing.TestManager;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class EditCardCommand extends Command {
 
@@ -22,6 +26,7 @@ public class EditCardCommand extends Command {
 
     private EditCardParser parser;
     private Deck deck;
+    private static Logger logger = Logger.getLogger(TestManager.class.getName());
 
     public EditCardCommand(String arguments, Deck deck) {
         super("EditCardCommand", arguments);
@@ -29,63 +34,83 @@ public class EditCardCommand extends Command {
         this.parser = new EditCardParser();
     }
 
+    public static String prepareCardIndex(String card, Deck deck) throws CardLiException {
+        logger.setLevel(Level.WARNING);
+        logger.log(Level.INFO, "preparing Card Index");
+        int cardIndex = 0;
+        if (Parser.isInteger(card)) {
+            logger.log(Level.INFO, "checking if integer cardIndex is out of bounds");
+            //card is an index
+            cardIndex = Integer.parseInt(card) - 1;
+            if (!(cardIndex >= 0 && cardIndex <= deck.getCards().size())) {
+                throw new CardLiException(INVALID_INDEX_ERROR_MESSAGE);
+            }
+        } else {
+            logger.log(Level.INFO, "Checking if String cardIndex exists in deck");
+            //card is a string input corresponding to front of the flashcard
+            boolean cardFound = false;
+            for (FlashCard c: deck.getCards()) {
+                if (c.getFront().equalsIgnoreCase(card)) {
+                    //card now is a string type containing index of card to be edited
+                    //assume no duplicate cards
+                    card = String.valueOf(deck.getCardIndex(c) + 1);
+                    cardFound = true;
+                }
+            }
+            if (!cardFound) {
+                throw new CardLiException(NO_SUCH_CARD_ERROR_MESSAGE);
+            }
+        }
+
+        return card;
+    }
+
+    public String[] prepareEditCardCommand() throws CardLiException {
+        logger.setLevel(Level.WARNING);
+        logger.log(Level.INFO, "preparing EditCardCommand");
+        logger.log(Level.INFO, "Checking if input contains /c, /s and /i");
+        if (!arguments.toLowerCase().contains("/c") || !arguments.toLowerCase().contains("/s")
+                || !arguments.toLowerCase().contains("/i")) {
+            throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
+        }
+        logger.log(Level.INFO, "Checking if /c,/s and /i are in the right order");
+        if (!(arguments.indexOf("/c") < arguments.indexOf("/s")
+                && arguments.indexOf("/s") < arguments.indexOf("/i"))) {
+            throw new InvalidCommandFormatException(WRONG_ORDER_ERROR_MESSAGE);
+        }
+        logger.log(Level.INFO, "Splitting the input up");
+        // "", card, side, input
+        String[] rawParameters = parser.parseArguments(super.arguments);
+        logger.log(Level.INFO, "Checking if there is enough arguments");
+        if (rawParameters.length < 4) {
+            throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
+        }
+
+        String card = rawParameters[1].trim();
+        String side = rawParameters[2].trim();
+        String input = rawParameters[3].trim();
+        logger.log(Level.INFO, "Checking if any field is empty");
+        if (card.isEmpty() || side.isEmpty() || input.isEmpty()) {
+            throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
+        }
+
+        logger.log(Level.INFO, "checking only front and back inputted to /s");
+        if (!(side.equalsIgnoreCase("front") | side.equalsIgnoreCase("back"))) {
+            throw new CardLiException(INVALID_SIDE_ERROR_MESSAGE);
+        }
+
+        logger.log(Level.INFO, "preparing CardIndex");
+        card = prepareCardIndex(card, deck);
+
+        String[] preparedArguments = {card, side, input};
+        return preparedArguments;
+    }
+
     @Override
     public CommandResult execute() { //edit /c <index> /s <side> /i <input>
         CommandResult result;
         try {
-            if (!arguments.toLowerCase().contains("/c") || !arguments.toLowerCase().contains("/s")
-                    || !arguments.toLowerCase().contains("/i")) {
-                throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
-            }
-            if (!(arguments.indexOf("/c") < arguments.indexOf("/s")
-                    && arguments.indexOf("/s") < arguments.indexOf("/i"))) {
-                throw new InvalidCommandFormatException(WRONG_ORDER_ERROR_MESSAGE);
-            }
-
-            // "", card, side, input
-            String[] rawParameters = parser.parseArguments(super.arguments);
-
-            if (rawParameters.length < 4) {
-                throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
-            }
-
-            String card = rawParameters[1].trim();
-            String side = rawParameters[2].trim();
-            String input = rawParameters[3].trim();
-
-            if (card.isEmpty() || side.isEmpty() || input.isEmpty()) {
-                throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
-            }
-
-            if (!(side.equalsIgnoreCase("front") | side.equalsIgnoreCase("back"))) {
-                throw new CardLiException(INVALID_SIDE_ERROR_MESSAGE);
-            }
-
-            int cardIndex = 0;
-            if (Parser.isInteger(card)) {
-                //card is an index
-                cardIndex = Integer.parseInt(card) - 1;
-                if (!(cardIndex >= 0 && cardIndex <= this.deck.getCards().size())) {
-                    throw new CardLiException(INVALID_INDEX_ERROR_MESSAGE);
-                }
-            } else {
-                //card is a string input corresponding to front of the flashcard
-                boolean cardFound = false;
-                for (FlashCard c: deck.getCards()) {
-                    if (c.getFront().equalsIgnoreCase(card)) {
-                        //card now is a string type containing index of card to be edited
-                        //assume no duplicate cards
-                        card = String.valueOf(deck.getCardIndex(c) + 1);
-                        cardFound = true;
-                    }
-                }
-                if (!cardFound) {
-                    throw new CardLiException(NO_SUCH_CARD_ERROR_MESSAGE);
-                }
-            }
-
-            String[] parameters = {card, side, input};
-
+            String[] parameters = prepareEditCardCommand();
             result = new CommandResult(deck.editCard(parameters));
         } catch (CardLiException e) {
             result = new CommandResult(e.getMessage());
--- src/main/java/seedu/duke/commands/deck/MoveCardCommand.java
+++ src/main/java/seedu/duke/commands/deck/MoveCardCommand.java
@@ -2,14 +2,17 @@ package seedu.duke.commands.deck;
 
 import seedu.duke.commands.Command;
 import seedu.duke.commands.CommandResult;
+import seedu.duke.commands.system.EditDeckCommand;
 import seedu.duke.exceptions.CardLiException;
 import seedu.duke.exceptions.FieldEmptyException;
 import seedu.duke.exceptions.InvalidCommandFormatException;
 import seedu.duke.flashcard.Deck;
 import seedu.duke.flashcard.DeckManager;
-import seedu.duke.flashcard.FlashCard;
-import seedu.duke.parser.Parser;
 import seedu.duke.parser.system.MoveCardParser;
+import seedu.duke.testing.TestManager;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MoveCardCommand extends Command {
 
@@ -17,15 +20,12 @@ public class MoveCardCommand extends Command {
             + "Format should be\n move /c <card index/front phrase of card> /d <deck index/name of deck>";
     private static final String WRONG_ORDER_ERROR_MESSAGE = "Incorrect move command! Format should be\n"
             + "move /c <card index/front phrase of card> /d <deck index/name of deck>";
-    private static final String INVALID_CARD_INDEX_ERROR_MESSAGE = "Incorrect index for Card!";
-    private static final String NO_SUCH_CARD_ERROR_MESSAGE = "No such card of that description exist!";
     private static final String SAME_DESTINATION_ERROR_MESSAGE = "Your card is already in the deck specified!";
-    private static final String INVALID_DECK_INDEX_ERROR_MESSAGE = "Incorrect index for deck!";
-    private static final String NO_SUCH_DECK_ERROR_MESSAGE = "No deck goes by that name!";
 
     private MoveCardParser parser;
     private Deck deck;
     private DeckManager deckManager;
+    private static Logger logger = Logger.getLogger(TestManager.class.getName());
 
     public MoveCardCommand(String arguments, Deck deck, DeckManager deckManager) {
         super("EditCardCommand", arguments);
@@ -34,85 +34,53 @@ public class MoveCardCommand extends Command {
         this.parser = new MoveCardParser();
     }
 
+    public String[] prepareMoveCommand() throws CardLiException {
+        logger.setLevel(Level.WARNING);
+        logger.log(Level.INFO, "preparing MoveCommand");
+        logger.log(Level.INFO, "Checking if input contains /d and /c");
+        if (!arguments.toLowerCase().contains("/c") || !arguments.toLowerCase().contains("/d")) {
+            throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
+        }
+        logger.log(Level.INFO, "Checking if /d and /c are in the right order");
+        if (!(arguments.indexOf("/c") < arguments.indexOf("/d"))) {
+            throw new InvalidCommandFormatException(WRONG_ORDER_ERROR_MESSAGE);
+        }
+        logger.log(Level.INFO, "Splitting the input up");
+        // "", card, deck
+        String[] rawParameters = parser.parseArguments(super.arguments);
+        logger.log(Level.INFO, "Checking if there is enough arguments");
+        if (rawParameters.length < 3) {
+            throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
+        }
+
+        String cardInput = rawParameters[1].trim();
+        String deckInput = rawParameters[2].trim();
+
+        logger.log(Level.INFO, "Checking if any field is empty");
+        if (cardInput.isEmpty() || deckInput.isEmpty()) {
+            throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
+        }
+        logger.log(Level.INFO, "preparing CardIndex and DeckIndex");
+        cardInput = EditCardCommand.prepareCardIndex(cardInput, deck);
+        deckInput = EditDeckCommand.prepareDeckIndex(deckInput, deckManager);
+
+        logger.log(Level.INFO, "checking if user is already in specified deck");
+        if ((Integer.parseInt(deckInput) - 1) == deckManager.getDeckIndex(deck)) {
+            throw new CardLiException(SAME_DESTINATION_ERROR_MESSAGE);
+        }
+
+        String sourceDeckIndex = String.valueOf(deckManager.getDeckIndex(deck));
+
+        String[] preparedArguments = {sourceDeckIndex, cardInput, deckInput};
+
+        return preparedArguments;
+    }
+
     @Override
     public CommandResult execute() { //move /c <index> /d <index
         CommandResult result;
         try {
-            if (!arguments.toLowerCase().contains("/c") || !arguments.toLowerCase().contains("/d")) {
-                throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
-            }
-            if (!(arguments.indexOf("/c") < arguments.indexOf("/d"))) {
-                throw new InvalidCommandFormatException(WRONG_ORDER_ERROR_MESSAGE);
-            }
-
-            // "", card, deck
-            String[] rawParameters = parser.parseArguments(super.arguments);
-
-            if (rawParameters.length < 3) {
-                throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
-            }
-
-            String cardInput = rawParameters[1].trim();
-            String deckInput = rawParameters[2].trim();
-
-            if (cardInput.isEmpty() || deckInput.isEmpty()) {
-                throw new FieldEmptyException(FIELD_EMPTY_ERROR_MESSAGE);
-            }
-            //checks if card to be moved is legit
-            int cardIndex = 0;
-            if (Parser.isInteger(cardInput)) {
-                //card is an index
-                cardIndex = Integer.parseInt(cardInput) - 1;
-                if (!(cardIndex >= 0 && cardIndex <= this.deck.getCards().size())) {
-                    throw new CardLiException(INVALID_CARD_INDEX_ERROR_MESSAGE);
-                }
-            } else {
-                //card is a string input corresponding to front of the flashcard
-                boolean cardFound = false;
-                for (FlashCard c: deck.getCards()) {
-                    if (c.getFront().equalsIgnoreCase(cardInput)) {
-                        //card now is a string type containing index of card to be edited
-                        //assume no duplicate cards
-                        cardInput = String.valueOf(deck.getCardIndex(c) + 1);
-                        cardFound = true;
-                    }
-                }
-                if (!cardFound) {
-                    throw new CardLiException(NO_SUCH_CARD_ERROR_MESSAGE);
-                }
-            }
-            //checks if deck to move card to is legit
-            int destinationDeckIndex = 0;
-            if (Parser.isInteger(deckInput)) {
-                //deckInput is an index
-                destinationDeckIndex = Integer.parseInt(deckInput) - 1;
-                if (!(destinationDeckIndex >= 0 && destinationDeckIndex <= this.deckManager.getDecksSize())) {
-                    throw new CardLiException(INVALID_DECK_INDEX_ERROR_MESSAGE);
-                }
-            } else {
-                //deckInput is a string input corresponding to name of the deck
-                boolean deckFound = false;
-                for (Deck d: deckManager.getDecks()) {
-                    if (d.getName().equalsIgnoreCase(deckInput)) {
-                        //deck now is a string type containing index of deck for card to be sent to
-                        //assume no duplicate decks
-                        deckInput = String.valueOf(deckManager.getDeckIndex(d) + 1);
-                        deckFound = true;
-                    }
-                }
-                if (!deckFound) {
-                    throw new CardLiException(NO_SUCH_DECK_ERROR_MESSAGE);
-                }
-            }
-
-            if ((Integer.parseInt(deckInput) - 1) == deckManager.getDeckIndex(deck)) {
-                throw new CardLiException(SAME_DESTINATION_ERROR_MESSAGE);
-            }
-
-            String sourceDeckIndex = String.valueOf(deckManager.getDeckIndex(deck));
-
-            String[] parameters = {sourceDeckIndex, cardInput, deckInput};
-
+            String[] parameters = prepareMoveCommand();
             result = new CommandResult(deckManager.moveCard(parameters));
         } catch (CardLiException e) {
             result = new CommandResult(e.getMessage());
