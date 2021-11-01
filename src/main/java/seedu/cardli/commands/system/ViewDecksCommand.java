df6abc1afc546d17dd5451dd7f92fdbf66a6d847
DESKTOP-G7UTADU\xk_20 <xkisxk@gmail.com>
2021-11-01 17:03:39 +0800

    Fixed bug where commands with no input accepts input

--- src/main/java/seedu/cardli/commands/system/ViewDecksCommand.java
+++ src/main/java/seedu/cardli/commands/system/ViewDecksCommand.java
@@ -8,13 +8,16 @@ public class ViewDecksCommand extends Command {
 
     private DeckManager deckManager;
 
-    public ViewDecksCommand(DeckManager deckManager) {
-        super("ViewDecksCommand");
+    public ViewDecksCommand(DeckManager deckManager, String arguments) {
+        super("ViewDecksCommand", arguments);
         this.deckManager = deckManager;
     }
 
     @Override
     public CommandResult execute() {
+        if (arguments.length() > 0) {
+            return new CommandResult("There should not be any arguments.");
+        }
         CommandResult result = new CommandResult(this.deckManager.viewDecks());
         return result;
     }
